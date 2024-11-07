71763011cf9830779c2386c54ecb149503e223af
Tigran Tumanyan <tigrantumanyann@gmail.com>
2024-11-07 18:22:30 -0500

    modification.

--- recursion/palindrome.lisp
+++ recursion/palindrome.lisp
@@ -6,3 +6,14 @@
      (let ((len (length word)))
        (if (equal (aref word 0) (aref word (- len 1))) (palindrome (subseq word 1 (- len 1)))
            nil)))))
+
+(defun list-palindrome (list-struct &optional (accumulator t))
+  (cond
+    ((equal accumulator nil) nil)
+    ((or (= (length list-struct) 0) (null list-struct) (= (length list-struct) 1)) t)
+    (t
+     (let ((len (length list-struct)))
+       (progn
+         (setf accumulator (equal (first list-struct) (elt list-struct (- len 1))))
+         (setf list-struct (subseq list-struct 1 (- len 1)))
+         (list-palindrome list-struct accumulator))))))
