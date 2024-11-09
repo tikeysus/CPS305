92a952e8f176e901bff93dd60b608909a3b08c75
Tigran Tumanyan <tigrantumanyann@gmail.com>
2024-11-08 23:30:23 -0500

    deleting to come up with solution afresh.

--- recursion/list-flattener.lisp
+++
@@ -1,6 +0,0 @@
-(defun list-flattener (list-struct)
-     (cond
-       ((null list-struct) list-struct)
-       ((typep (first list-struct) 'list)
-        (append (list-flattener (first list-struct)) (list-flattener (rest list-struct))))
-       (t (cons (first list-struct) (list-flattener (rest list-struct))))))
