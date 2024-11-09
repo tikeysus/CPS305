e6974063f746c75de88a05200873a806d9f810ea
Tigran Tumanyan <tigrantumanyann@gmail.com>
2024-11-08 21:04:38 -0500

    deleted file.

--- recursion/max-elem.lisp
+++
@@ -1,6 +0,0 @@
-(defun max-elem (lst &optional (accumulator 0))
-  (cond
-    ((null lst) accumulator)
-    (t (progn
-         (if (< accumulator (first lst)) (setf accumulator (first lst)))
-         (max-elem (rest lst) accumulator)))))
