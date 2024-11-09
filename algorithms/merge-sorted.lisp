39b21d3d0a0a56579c26506b6b22985156cd893f
Tigran Tumanyan <tigrantumanyann@gmail.com>
2024-11-09 10:58:32 -0500

    bad code, but gets the job done.

--- algorithms/merge-sorted.lisp
+++ algorithms/merge-sorted.lisp
@@ -1,23 +1,41 @@
-(defun merge-sorted (list1 list2 comparator) ;assumes two sorted lists as input
+(defun merge-sorted (list1 list2 comparator)
   (cond
     ((null list1) list2)
     ((null list2) list1)
     ((and (null list1) (null list2)) nil)
-    (t
-     (do ((stack (make-list 0)))
-          ((or (null (first list1)) (null (first list2)))
-           (progn
-             (dolist (element list1)
-               (setf stack (append stack (list element)))
-               (pop list1))
-             (dolist (element list2)
-               (setf stack (append stack (list element)))
-               (pop list2))
-             stack))
-       (if (funcall comparator (first list1) (first list2))
-           (progn
-             (setf stack (append stack (list (first list1))))
-             (pop list1))
-           (progn
-             (setf stack (append stack (list (first list2))))
-             (pop list2)))))))
+    ((funcall comparator 1 2) (merge-ascending list1 list2 comparator))
+    (t (merge-descending list1 list2 comparator))))
+
+(defun merge-ascending (list1 list2 comparator)
+  (let ((stack (make-list 0)))
+    (do () ((or (null list1) (null list2))
+            (progn
+              (dolist (element list1)
+                (append stack (list element)))
+              (dolist (element list2)
+                (append stack (list element)))
+              stack))
+      (if (funcall comparator (first list1) (first list2))
+          (progn
+            (setf stack (append stack (list (first list1))))
+            (pop list1))
+          (progn
+            (setf stack (append stack (list (first list2))))
+            (pop list2))))))
+
+(defun merge-descending (list1 list2 comparator)
+  (let ((stack (make-list 0)))
+    (do () ((or (null list1) (null list2))
+            (progn
+              (dolist (element list1)
+                (push element stack))
+              (dolist (element list2)
+                (push element stack))
+              stack))
+      (if (not (funcall comparator (first list1) (first list2)))
+          (progn
+            (setf stack (push (first list1) stack))
+            (pop list1))
+          (progn
+            (setf stack (push (first list2) stack))
+            (pop list2))))))
