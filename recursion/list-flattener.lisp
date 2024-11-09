c4eb2ea44f20d23299fc6ddffaa9d3960991e890
Tigran Tumanyan <tigrantumanyann@gmail.com>
2024-11-09 10:58:00 -0500

    solved it once more.

---
+++ recursion/list-flattener.lisp
@@ -0,0 +1,5 @@
+(defun list-flattener (list-struct &optional (accumulator nil))
+  (cond
+    ((null list-struct) accumulator)
+    ((listp (first list-struct)) (list-flattener (first list-struct) accumulator))
+    (t (list-flattener (rest list-struct) (append accumulator (list (first list-struct)))))))
